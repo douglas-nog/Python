747cf01a9f3449e8ddd8068a0f7beb5b1cc804c7
douglas-nog <douglaswnogueira@hotmail.com>
2022-09-26 21:48:06 -0300

    Jogo de advinhação em python

---
+++ Jogo_advinhar_com_contador.py
@@ -0,0 +1,20 @@
+#O computador vai "pensar" em um número entre 0 e 10. Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer.
+
+from random import randint
+computador = randint(0, 100)
+print('Sou seu computador e acabei de pensar em um número de 0 a 100.')
+print('Será que você consegue advinhar?')
+acertou = False
+palpites = 0
+while not acertou:
+    jogador = int(input('Qual a sua escolha? '))
+    palpites += 1
+    if jogador == computador:
+        acertou = True
+    else:
+        if jogador < computador:
+            print('Maior, tente novamente: ')
+        elif jogador > computador:
+            print('Menor, tente novamente')
+print('Você acertou após {} tentativas.'.format(palpites))
+
